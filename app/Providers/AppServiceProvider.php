6ba416682e83c960df53ad8195cd19e99b0ac058
MIR HB RAHMAN <ahadcr0@gmail.com>
2020-04-10 20:05:21 +0600

    Braintree integration

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    { 
+
+        \Braintree\Configuration::environment(config('services.braintree.environment'));
+        \Braintree\Configuration::merchantId(config('services.braintree.marchant_id'));
+        \Braintree\Configuration::publicKey(config('services.braintree.public_key'));
+        \Braintree\Configuration::privateKey(config('services.braintree.private_key'));
+        
+    }
+}
